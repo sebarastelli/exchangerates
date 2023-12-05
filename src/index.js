f3186614684d927d9d76e1c1d7a1eb1c22119ab0
Sebastián Rastelli <seba-rastelli@hotmail.com>
2023-12-05 18:04:46 -0300

    Reparando bugs e implementando fecha

--- src/index.js
+++ src/index.js
@@ -3,64 +3,97 @@ const prueba = document.querySelector("#prueba");
 const titulo = document.querySelector("#titulo");
 const tipoDeCambio = document.querySelector("#tipoDeCambio");
 const total = document.querySelectorAll(".card-text");
-const cantidadInput = document.querySelector("#cantidadInput")
+const cantidadInput = document.querySelector("#cantidadInput");
+const fechaInput = document.querySelector("#fechaInput");
 let primerCambioElegido = "USD";
 let cantidad = 1;
+let fechaSeleccionada = "latest";
+let fechaFormateada;
 
-tipoDeCambio.addEventListener("change", ()=>{
-    primerCambioElegido = tipoDeCambio.value;
-    titulo.innerText = `Elegiste ${primerCambioElegido}`;
-    renderizadoDeCartas();
-})
+tipoDeCambio.addEventListener("change", () => {
+  primerCambioElegido = tipoDeCambio.value;
+  titulo.innerText = `Elegiste ${primerCambioElegido}`;
+  renderizadoDeCartas();
+});
 
 function comparacion(cambiosPredefinidos) {
-    
-    if (primerCambioElegido === cambiosPredefinidos) {
-        total.forEach(cardText => {
-            const cardTitle = cardText.parentElement.querySelector('.card-title').innerText;
-            if (cardTitle === primerCambioElegido) {
-                cardText.innerText = '';
-            }
+  if (primerCambioElegido === cambiosPredefinidos) {
+    total.forEach((cardText) => {
+      const cardTitle =
+        cardText.parentElement.querySelector(".card-title").innerText;
+      if (cardTitle === primerCambioElegido) {
+        cardText.innerText = "";
+      }
+    });
+    return;
+  }
+  if (fechaSeleccionada !== "latest") {
+    fetch(
+      `${URL}${fechaFormateada}?amount=${cantidad}&from=${primerCambioElegido}&to=${cambiosPredefinidos}`
+    )
+      .then((resp) => resp.json())
+      .then((data) => {
+        total.forEach((cardText) => {
+          const cardTitle =
+            cardText.parentElement.querySelector(".card-title").innerText;
+          if (cardTitle === cambiosPredefinidos) {
+            cardText.innerText = `${cantidad} ${primerCambioElegido} = ${data.rates[cambiosPredefinidos]} ${cambiosPredefinidos}`;
+          } else if (cardTitle === primerCambioElegido) {
+            cardText.innerText = "";
+          }
         });
-        return; 
-    }
+      })
+      .catch((error) => console.error("Error en la solicitud:", error));
+  }
 
-    fetch(`${URL}latest?amount=${cantidad}&from=${primerCambioElegido}&to=${cambiosPredefinidos}`)
-        .then(resp => resp.json())
-        .then((data) => {
-            total.forEach(cardText => {
-                const cardTitle = cardText.parentElement.querySelector('.card-title').innerText;
-                if (cardTitle === cambiosPredefinidos) {
-                    cardText.innerText = `${cantidad} ${primerCambioElegido} = ${data.rates[cambiosPredefinidos]} ${cambiosPredefinidos}`;
-                } else if (cardTitle === primerCambioElegido) {
-                    cardText.innerText = '';
-                }
-            });
-        })
-        .catch(error => console.error("Error en la solicitud:", error));
+  if (fechaSeleccionada === "latest") {
+    fetch(
+      `${URL}latest?amount=${cantidad}&from=${primerCambioElegido}&to=${cambiosPredefinidos}`
+    )
+      .then((resp) => resp.json())
+      .then((data) => {
+        total.forEach((cardText) => {
+          const cardTitle =
+            cardText.parentElement.querySelector(".card-title").innerText;
+          if (cardTitle === cambiosPredefinidos) {
+            cardText.innerText = `${cantidad} ${primerCambioElegido} = ${data.rates[cambiosPredefinidos]} ${cambiosPredefinidos}`;
+          } else if (cardTitle === primerCambioElegido) {
+            cardText.innerText = "";
+          }
+        });
+      })
+      .catch((error) => console.error("Error en la solicitud:", error));
+  }
 }
 
 function renderizadoDeCartas() {
-    let divisas = document.querySelectorAll(".card-title");
-    divisas.forEach((divisa) => {
-        let cambiosPredefinidos = divisa.innerText;
-        comparacion(cambiosPredefinidos);
-    });
+  let divisas = document.querySelectorAll(".card-title");
+  divisas.forEach((divisa) => {
+    let cambiosPredefinidos = divisa.innerText;
+    comparacion(cambiosPredefinidos);
+  });
 }
 
 function enviarCantidad() {
-    cantidadSeleccionada = cantidadInput.value;
-    if (!isNaN(cantidadSeleccionada) && cantidadSeleccionada.length <= 10) {
-        cantidad = cantidadInput.value;
-        renderizadoDeCartas();
-    } else {
-        alert('Por favor, ingrese un número válido de hasta 10 caracteres.');
-    }
+  cantidadSeleccionada = cantidadInput.value;
+  if (!isNaN(cantidadSeleccionada) && cantidadSeleccionada.length <= 10) {
+    cantidad = cantidadInput.value;
+    renderizadoDeCartas();
+  } else {
+    alert("Por favor, ingrese un número válido de hasta 10 caracteres.");
+  }
 }
 
-renderizadoDeCartas()
+function obtenerFecha() {
+  fechaSeleccionada = fechaInput.value;
+  if (fechaSeleccionada) {
+    fechaFormateada = new Date(fechaSeleccionada).toISOString().split("T")[0];
+    renderizadoDeCartas();
+  } else {
+    alert("Por favor, seleccione una fecha válida");
+  }
+}
 
+renderizadoDeCartas();
 
-
-// 
-
+//
