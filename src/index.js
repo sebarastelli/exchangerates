448f36ef4c4eebb9749f10f5f6818fbd6eb2d512
Sebastián Rastelli <seba-rastelli@hotmail.com>
2023-12-04 21:29:56 -0300

    Creación de interfaz funcional con número reducido de divisas

---
+++ src/index.js
@@ -0,0 +1,66 @@
+const URL = "https://api.frankfurter.app/";
+const prueba = document.querySelector("#prueba");
+const titulo = document.querySelector("#titulo");
+const tipoDeCambio = document.querySelector("#tipoDeCambio");
+const total = document.querySelectorAll(".card-text");
+const cantidadInput = document.querySelector("#cantidadInput")
+let primerCambioElegido = "USD";
+let cantidad = 1;
+
+tipoDeCambio.addEventListener("change", ()=>{
+    primerCambioElegido = tipoDeCambio.value;
+    titulo.innerText = `Elegiste ${primerCambioElegido}`;
+    renderizadoDeCartas();
+})
+
+function comparacion(cambiosPredefinidos) {
+    
+    if (primerCambioElegido === cambiosPredefinidos) {
+        total.forEach(cardText => {
+            const cardTitle = cardText.parentElement.querySelector('.card-title').innerText;
+            if (cardTitle === primerCambioElegido) {
+                cardText.innerText = '';
+            }
+        });
+        return; 
+    }
+
+    fetch(`${URL}latest?amount=${cantidad}&from=${primerCambioElegido}&to=${cambiosPredefinidos}`)
+        .then(resp => resp.json())
+        .then((data) => {
+            total.forEach(cardText => {
+                const cardTitle = cardText.parentElement.querySelector('.card-title').innerText;
+                if (cardTitle === cambiosPredefinidos) {
+                    cardText.innerText = `${cantidad} ${primerCambioElegido} = ${data.rates[cambiosPredefinidos]} ${cambiosPredefinidos}`;
+                } else if (cardTitle === primerCambioElegido) {
+                    cardText.innerText = '';
+                }
+            });
+        })
+        .catch(error => console.error("Error en la solicitud:", error));
+}
+
+function renderizadoDeCartas() {
+    let divisas = document.querySelectorAll(".card-title");
+    divisas.forEach((divisa) => {
+        let cambiosPredefinidos = divisa.innerText;
+        comparacion(cambiosPredefinidos);
+    });
+}
+
+function enviarCantidad() {
+    cantidadSeleccionada = cantidadInput.value;
+    if (!isNaN(cantidadSeleccionada) && cantidadSeleccionada.length <= 10) {
+        cantidad = cantidadInput.value;
+        renderizadoDeCartas();
+    } else {
+        alert('Por favor, ingrese un número válido de hasta 10 caracteres.');
+    }
+}
+
+renderizadoDeCartas()
+
+
+
+// 
+
